7e7207cf8888d42cee3c0f2e1b87551c75551676
Lady Stock <nikki.castlea@gmail.com>
2020-09-30 16:52:13 -0600

    beginning of countdown function

---
+++ assets/js/index.js
@@ -0,0 +1,19 @@
+let seconds;
+
+const countdown = () => {
+    const time = document.getElementById("time");
+    const timer=setInterval(timer, 1000); 
+
+    const countDown = () =>
+    {
+    time=time-1;
+    if (time <= 0)
+    {
+        clearInterval(counter);
+        //TODO: counter ended, do something here
+        return;
+    }
+
+    //TODO: code for showing the number of seconds here
+    }
+}
